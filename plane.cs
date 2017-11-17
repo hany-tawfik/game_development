de5809756eede5210aa1da72cbba00756876bf67
Hany Tawfik <hany-tawfik@hotmail.com>
2017-11-17 15:17:33 +0100

    Update plane.cs

--- plane.cs
+++ plane.cs
@@ -5,6 +5,41 @@ using MidiJack
 
 //Hany is trying to use functions
 
-public class plane : MonoBehaviour {
+public class plane : MonoBehaviour { //the class plane inherites class monobehaviour
+    
+    //Note name and equivalent midi number dictionary
+    Dictionary<string, int> dict = new Dictionary<string, int>();
+    dict.Add("C3", 60);
+    dict.Add("D3", 62);
+    dict.Add("E3", 64);
+    dict.Add("F3", 65);
+    dict.Add("G3", 67);
+    dict.Add("A3", 69);
+    dict.Add("B3", 71);
+    dict.Add("C4", 72);
+    dict.Add("D4", 74);
+    dict.Add("E4", 76);
+    dict.Add("F4", 77);
+    
+    //Lines and spaces coordinates
+    
+    E3
+    
+    // Use this for initialization
+    void Start () {
+              source = GetComponent<AudioSource>();
+      }
+    
+    
+    // Repositioning
+	  void new_position () {
+            transform.position = new Vector3(-1.47f, 0.582f, 1.14f);
+    }
+
+    
+    
+    
+
+  
 
 
