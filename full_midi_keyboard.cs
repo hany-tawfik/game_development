cf09d17d7ff52852b1bedf6fd17bd7a2b4033540
Hany Tawfik <hany-tawfik@hotmail.com>
2017-12-11 14:21:33 +0100

    Update full_midi_keyboard.cs

--- full_midi_keyboard.cs
+++ full_midi_keyboard.cs
@@ -1,1 +1,9 @@
-if (number>smallestNumber&number<largestNumber){
+if (i>0 & i<127){
+
+  
+  
+  if (MidiMaster.GetKeyDown(D3))
+        {
+            transform.position = new Vector3(-1.47f, 0.582f, 1.14f);// new position
+            OD3.SetActive(true); //green high lighting
+            source.PlayOneShot(MD3, 1.0f); //play sound
